9c9b952ca2c896b21abf70c736890f6c0979e543
Patrick Kolodziej <kolodziej@365farmnet.com>
2019-08-12 12:28:48 +0200

    Testing registration

--- componenttest/register_api_test.go
+++ componenttest/register_api_test.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"github.com/labstack/echo"
-	"lgn/internal/model"
 	"net/http"
 	"net/http/httptest"
 )
@@ -20,11 +19,36 @@ func (suite *ComponentTestSuite) TestRegisterUserSuccessful() {
 
 	suite.Equal(http.StatusCreated, recorder.Code)
 
-	var userVo model.UserVo
-	suite.Nil(json.NewDecoder(recorder.Body).Decode(&userVo))
+	var registerResponse map[string]interface{}
+	suite.Nil(json.NewDecoder(recorder.Body).Decode(&registerResponse))
 
-	suite.True(len(userVo.Id) > 0)
-	suite.Equal("kolo", userVo.Name)
-	suite.NotNil(userVo.Created)
-	suite.NotNil(userVo.Updated)
+	suite.True(len(registerResponse["id"].(string)) > 0)
+	suite.Equal("kolo", registerResponse["name"])
+	suite.NotNil(registerResponse["created"])
+	suite.NotNil(registerResponse["updated"])
+}
+
+func (suite *ComponentTestSuite) TestRegisterUserWithExistingShouldFail() {
+	firstBody := bytes.NewBuffer([]byte(`{"name": "kolo", "password": "Pass00"}`))
+
+	firstRecorder := httptest.NewRecorder()
+	firstRequest, _ := http.NewRequest("POST", "/api/register", firstBody)
+	firstRequest.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+	suite.router.ServeHTTP(firstRecorder, firstRequest)
+	suite.Equal(http.StatusCreated, firstRecorder.Code)
+
+	secondBody := bytes.NewBuffer([]byte(`{"name": "Kolo", "password": "Pass00"}`))
+
+	secondRecorder := httptest.NewRecorder()
+	secondRequest, _ := http.NewRequest("POST", "/api/register", secondBody)
+	secondRequest.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+	suite.router.ServeHTTP(secondRecorder, secondRequest)
+	suite.Equal(http.StatusBadRequest, secondRecorder.Code)
+
+	var registerResponse map[string]interface{}
+	suite.Nil(json.NewDecoder(secondRecorder.Body).Decode(&registerResponse))
+
+	suite.Equal("duplicate key value violates unique constraint \"user_name_uindex\"", registerResponse["message"])
 }
