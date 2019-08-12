49c937cc0d78ccf2be6176beb8fa0f94acc6d2df
Patrick Kolodziej <kolodziej@365farmnet.com>
2019-08-12 14:25:55 +0200

    Fix migrations

---
+++ scripts/run_migrations_ci.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Running dba migrations"
+./migrate --source file://../migrations/dba -database "postgres://kolo:Pass00@localhost:5432/postgres?sslmode=disable" up
+echo "Dba migrations finished"
+
+echo "Running lgn migrations"
+./migrate --source file://../migrations/lgn -database "postgres://lgn:lgn@localhost:5432/lgn_service?sslmode=disable" up
+echo "Lgn migrations finished"
