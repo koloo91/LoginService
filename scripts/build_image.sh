7846b6056c8f44b0b02177f19340f592cc64c704
Patrick Kolodziej <koloooo91@googlemail.com>
2019-08-13 22:10:26 +0200

    Building arm docker images

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 ./increment_version.sh -m "$(cat version)" > version
-docker build -t koloooo/lgn:"$(cat version)" -t koloooo/lgn:latest docker/x86
-docker build -t koloooo/lgn_arm:"$(cat version)" -t koloooo/lgn_arm:latest docker/arm
+#docker build -t koloooo/lgn:"$(cat version)" -t koloooo/lgn:latest -f docker/x86/Dockerfile ../.
+docker build -t koloooo/lgn_arm:"$(cat version)" -t koloooo/lgn_arm:latest -f docker/arm/Dockerfile ../.
 
-docker push koloooo/lgn:"$(cat version)"
-docker push koloooo/lgn:latest koloooo/lgn_arm:"$(cat version)" koloooo/lgn_arm:latest
+#docker push koloooo/lgn:"$(cat version)"
+#docker push koloooo/lgn:latest
 docker push koloooo/lgn_arm:"$(cat version)"
 docker push koloooo/lgn_arm:latest
