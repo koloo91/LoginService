069d4021c091c433ce5e5feea623e0bcbd38b4f1
Patrick Kolodziej <koloooo91@googlemail.com>
2019-08-19 22:03:33 +0200

    Add custom context

---
+++ app/user_context.go
@@ -0,0 +1,15 @@
+package app
+
+import (
+	"bitbucket.org/Koloo/lgn/app/model"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo"
+)
+
+type UserContext struct {
+	echo.Context
+}
+
+func (ctx *UserContext) GetUser() model.UserClaim {
+	return ctx.Get("user").(*jwt.Token).Claims.(model.UserClaim)
+}
